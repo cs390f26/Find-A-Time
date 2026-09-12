c9aff0c86858dc9cae168fd45015021289b5efbc
Mohammad Alzoubi <alzoubim@moravian.edu>
2026-09-12 13:36:37 -0400

    Create userdata.sh for environment setup and service
    Add a script to set up the environment and service for the application.

--- deploy/userdata.sh
+++ deploy/userdata.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y python3 git
+git clone https://github.com/cs390f26/find-a-time-monolith.git /home/ec2-user/find-a-time-monolith
+cd /home/ec2-user/find-a-time-monolith
+python3 -m venv .venv
+.venv/bin/pip install -e .
+sudo cp deploy/find-a-time.service /etc/systemd/system/
+sudo systemctl enable find-a-time
+sudo systemctl start find-a-time
